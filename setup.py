c93c69e76d0251146b1488a2e3c3e1c83dc1d412
Carlos Guirado <carlosguirado@users.noreply.github.com>
2024-10-21 22:32:11 -0700

    PyPI release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mcbs",
-    version="0.1.0",
+    version="0.1.1",
     author="Carlos Guirado",
     author_email="guirado@berkeley.edu",
     description="A benchmarking sandbox for mode choice models",
